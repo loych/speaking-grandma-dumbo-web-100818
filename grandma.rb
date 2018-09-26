448ba94d3dee97d2f5638d2db10f1b01793ab394
IS Choi <insil321@hotmail.com>
2018-09-26 10:05:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,5 +18,6 @@ if phrase== phrase.upcase
 puts "NO, NOT SINCE 1938!"
 elseif phrase=="I LOVE YOU GRANDMA!"
 puts "I LOVE YOU TOO PUMPKIN!"
-else puts "HUH?! SPEAK UP, SONNY!"
+else 
+  puts "HUH?! SPEAK UP, SONNY!"
 end
